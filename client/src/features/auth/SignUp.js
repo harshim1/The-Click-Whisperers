2125fc22d4dad918b28df143cea51377e852c854
khushilad <120996371+mangoinatree@users.noreply.github.com>
2024-06-26 10:57:46 -0600

    Sign-Up page

---
+++ client/src/features/auth/SignUp.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styles from './signup.module.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const SignUp = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+  return (
+    <section>
+       <div className={styles.background}>
+            <img src = '/assets/background2.svg' alt='backgroundSVG' className={styles.backgroundImage}></img>
+        </div> 
+        <div className={styles.content}>
+           <h3>Create an Account</h3>
+            <form>
+                <input
+                type="text" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder='Email'
+                />
+                <input
+                type="text" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password'
+                />
+                <button type="submit" className={styles.submitButton}>get started</button>
+            </form> 
+            <div className={styles.login}>
+               <p>Already have an account? <Link>Log In</Link></p> 
+            </div>
+            
+        </div>
+        
+    </section>
+    
+  )
+}
+
+export default SignUp
